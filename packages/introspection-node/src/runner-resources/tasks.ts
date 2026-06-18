2836316580277560e85fb43444fd0d05fdf1c9ea
Julian <brightsparc@gmail.com>
2026-06-18 15:37:00 -0700

    Remove node tasks re-export facade
    Now imported directly from @introspection-sdk/http.

--- packages/introspection-node/src/runner-resources/tasks.ts
+++
@@ -1,8 +0,0 @@
-export {
-  RunHandle,
-  TaskRunsApi,
-  TaskRunsClient,
-  TasksApi,
-  TasksClient,
-} from "@introspection-sdk/http";
-export type { StartParams } from "@introspection-sdk/http";
